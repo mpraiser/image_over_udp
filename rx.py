1bea34a764f4ced84c1d7aa63562bae0ebe388eb
mraiser <mraiser@foxmail.com>
2021-07-02 17:34:54 +0800

    use threading to print image in a nonblocking way

--- rx.py
+++ rx.py
@@ -1,5 +1,11 @@
 import cv2
+import threading
 from transceiver import Transceiver, FragFailure
+
+
+def show(c, i):
+    cv2.imshow(str(c), i)
+    cv2.waitKey(0)
 
 
 if __name__ == "__main__":
@@ -8,8 +14,7 @@ if __name__ == "__main__":
     while True:
         try:
             image = rx.recv_image(("127.0.0.1", 8000))
-            cv2.imshow(str(counter), image)
-            cv2.waitKey(0)
+            threading.Thread(target=show, args=(counter, image), daemon=True).start()
             counter += 1
         except FragFailure:
             print("组包或接收失败")
